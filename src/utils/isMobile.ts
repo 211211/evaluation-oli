859706b4f7121db3fac6d530f2ee796c6f3a333d
211211 <nhquan307@outlook.com>
2020-07-24 19:16:58 +0700

    Add pre-commit hook for lint-stated

--- src/utils/isMobile.ts
+++ src/utils/isMobile.ts
@@ -1,5 +1,3 @@
 export const isMobile = (): boolean => {
-    const isMobile: boolean = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
-
-    return isMobile
+    return /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
 }
